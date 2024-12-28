770037b78e334251c11818731bf9c0698662bf29
tguilfoos <82964838+tguilfoos@users.noreply.github.com>
2024-12-28 15:39:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,7 +92,7 @@ if not len(countries):
 selected_countries = st.multiselect(
     'Which countries would you like to view?',
     countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
+    ['USA'])
 
 ''
 ''
